b5fb518ffc286a0c362171892f449c2f26dd624e
ertrzyiks <mateusz.derks@gmail.com>
2020-06-05 19:43:08 +0200

    Add more than 50 annotations

--- script.js
+++ script.js
@@ -12,18 +12,59 @@ module.exports = async ({context, github}) => {
         message: `[0] This is lame`
     })
 
-    await github.checks.create({
+    annotations.push({
+        path: 'index.js',
+        start_line: 2,
+        end_line: 2,
+        annotation_level: '', // failure
+        message: `[0] This is lame`
+    })
+
+    const { data }  = await github.checks.create({
         owner,
         repo,
         name: 'Test check',
         started_at: new Date(),
         status: 'completed',
         conclusion: annotations.length > 0 ? 'failure' : 'success',
-        annotations,
         output: {
             title: 'This is the check title',
             summary: `0 error(s), 0 warning(s) found`,
             annotations
+        },
+        head_sha: sha
+    })
+
+    const { id } = data
+
+    await github.checks.update({
+        check_run_id: id,
+        owner,
+        repo,
+        name: 'Test check',
+        started_at: new Date(),
+        status: 'completed',
+        conclusion: annotations.length > 0 ? 'failure' : 'success',
+        output: {
+            title: 'This is the check title',
+            summary: `0 error(s), 0 warning(s) found`,
+            annotations: [annotations[0]]
+        },
+        head_sha: sha
+    })
+
+    await github.checks.update({
+        check_run_id: id,
+        owner,
+        repo,
+        name: 'Test check',
+        started_at: new Date(),
+        status: 'completed',
+        conclusion: annotations.length > 0 ? 'failure' : 'success',
+        output: {
+            title: 'This is the check title',
+            summary: `0 error(s), 0 warning(s) found`,
+            annotations: [annotations[1]]
         },
         head_sha: sha
     })
