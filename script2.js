02c8e63fbaa271e76581e9a30a94e041cca18a51
ertrzyiks <mateusz.derks@gmail.com>
2020-06-06 21:52:21 +0200

    Add deployment

---
+++ script2.js
@@ -0,0 +1,10 @@
+module.exports = async ({context, github}) => {
+    const { sha } = context
+    const { owner, repo } = context.repo
+
+    github.repos.createDeployment({
+        owner,
+        repo,
+        ref: sha
+    })
+}
